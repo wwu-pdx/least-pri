380a263fef7b826d87109f6e27320fed3eed7106
Wenjing Wu <wwu@pdx.edu>
2020-03-24 03:04:39 -0700

    add function back

--- core/levels/leastprivilege/c1bucket/function/main.py
+++ core/levels/leastprivilege/c1bucket/function/main.py
@@ -39,9 +39,10 @@ def main(request):
 		# If the service account has any of the permissions, add them to the output list
 		if 'permissions' in response:
 			given_permissions.extend(response['permissions'])
+        return "<p>Not least privilege, please try again! "+given_permissions+"</p>"
 
-	diff=list(set(given_permissions) - set(lst_pri))
-	if len(diff)==0:	
-		return "<p>congratulations!</p>"
-	else:
-		return "<p>Not least privilege, please try again! "+given_permissions+"</p>"
+	#diff=list(set(given_permissions) - set(lst_pri))
+	#if len(diff)==0:	
+	#	return "<p>congratulations!</p>"
+	#else:
+	#	return "<p>Not least privilege, please try again! "+given_permissions+"</p>"
