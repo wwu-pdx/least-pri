753960d5bf49974fada45984de0d1d307514f54f
Wenjing Wu <wwu@pdx.edu>
2020-04-24 09:27:20 -0700

    update templates

--- core/levels/leastprivilege/c1bucket/functionaccess/main.py
+++ core/levels/leastprivilege/c1bucket/functionaccess/main.py
@@ -4,7 +4,7 @@ def main(request):
 	import google.oauth2.service_account
 	from google.oauth2.credentials import Credentials
 	import os
-	from cryptography.fernet import Fernet
+	
 	
 	# Only one of the following need to be set:
 	SERVICE_ACCOUNT_KEY_FILE = 'c1-access.json'
@@ -12,12 +12,10 @@ def main(request):
 	
 	# Set the project ID
 	PROJECT_ID = os.environ['GCP_PROJECT']
+	FUNCTION_REGION = os.environ['FUNCTION_REGION']
+	NONCE = os.environ.get('NONCE', 'Specified environment variable is not set.')
 	
-	# Get function env variable
-	key = os.environ.get('fvar2', 'Specified environment variable is not set.').encode("utf-8") 
-	fvar1 = os.environ.get('fvar1', 'Specified environment variable is not set.').encode("utf-8") 
-	f = Fernet(key)
-	PRI = f.decrypt(fvar1).decode("utf-8") 
+
 
 	credentials = google.oauth2.service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_KEY_FILE)
 
@@ -25,17 +23,17 @@ def main(request):
 	storage_api = discovery.build('storage', 'v1', credentials=credentials)
 	try:
 		buckets = storage_api.buckets().list(project=PROJECT_ID).execute()["items"][0]["name"]
-	except:
-		buckets = ''
-	
-	given_permissions.sort()	
-	re=',\n'.join(str(x) for x in given_permissions)
-	
-	pri="".join(PRI.split()).split(',')
-	pri.sort()
+	except Exception as e:
+		buckets = str(e)
+
 
 	if buckets == '':
-		return "No bucket listed. Insufficient privilege!\n Your current testable permissions are:\n ["+re+"]"
+		buckets = "No bucket listed. Insufficient privilege!"
 	
-	return render_template('c1.html', pri=pri, buckets=buckets)
+	url=f'https://{FUNCTION_REGION}-{PROJECT_ID}.cloudfunctions.net/c1-func-edit-{NONCE}'
+	
+	
+	return render_template('c1.html', bucket=buckets, url=url)
+
+	
 
--- core/levels/leastprivilege/c1bucket/functionedit/main.py
+++ core/levels/leastprivilege/c1bucket/functionedit/main.py
@@ -1,19 +1,11 @@
+from flask import render_template
 def main(request):
 	from googleapiclient import discovery
 	import google.oauth2.service_account
 	from google.oauth2.credentials import Credentials
 	import os
-	
-	# request_json = request.get_json(silent=True)
-	# request_args = request.args
-	# if request_json and 'permissions' in request_json:
-		# permissions = request_json['permissions']
-	# elif request_args and 'permissions' in request_args:
-		# permissions = request_args['permissions'].split(',')
-	# else:
-		# #Set to oringal permissions
-		# permissions = ['storage.buckets.list','storage.objects.list','storage.buckets.delete','iam.roles.get']
-	# Set account key file:
+	from cryptography.fernet import Fernet
+
 	SERVICE_ACCOUNT_KEY_FILE = 'c1-edit.json'
 
 	
@@ -24,23 +16,40 @@ def main(request):
 	NONCE = os.environ.get('NONCE', 'Specified environment variable is not set.')
 	
 
+	key = os.environ.get('fvar2', 'Specified environment variable is not set.').encode("utf-8") 
+	fvar1 = os.environ.get('fvar1', 'Specified environment variable is not set.').encode("utf-8") 
+	f = Fernet(key)
+	PRI = f.decrypt(fvar1).decode("utf-8") 
+	
+	pri="".join(PRI.split()).split(',')
+	pri.sort()
+
 	credentials = google.oauth2.service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_KEY_FILE)
 
 
-	# Build iam REST API python object
-	iam_api = discovery.build('iam','v1', credentials=credentials)
+	# Build cloudresourcemanager REST API python object
+	service = discovery.build('iam','v1', credentials=credentials)
 
 
 	name = f'projects/{PROJECT_ID}/roles/c1_access_role_{NONCE}'  
 
 	per=''
+	rolename=''
 	try:
-		roles = iam_api.projects().roles().get(name=name).execute()
-		per=roles['name']+':  '
-		print(roles['name'])
-		for permission in roles['includedPermissions']:
-			per += permission+'   '
-			print(permission)
+		roles = service.projects().roles().get(name=name).execute()
+		rolename = roles['name']
+		#print(roles['name'])
+		per = roles['includedPermissions'].sort()
+		# for permission in roles['includedPermissions']:
+			# per += permission+'   '
+			# #print(permission)
 	except Exception as e: 
 		per =str(e)
-	return per
+	
+	msg=''
+	if ''.join(per) == ''.join(pri):
+		msg='Congratulations! You get the least privileges. '
+	else:
+		msg='Not least privilege, please try again!'
+
+	return render_template('c1-permission.html', pri=pri, per=per, msg=msg, rn=rolename)
