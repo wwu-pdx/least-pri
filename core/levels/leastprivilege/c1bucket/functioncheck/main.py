905961ebb05291943974a89b576e937d83e323b1
Wenjing Wu <wwu@pdx.edu>
2020-05-08 06:51:06 -0700

    debug

--- core/levels/leastprivilege/c1bucket/functioncheck/main.py
+++ core/levels/leastprivilege/c1bucket/functioncheck/main.py
@@ -11,7 +11,7 @@ def main(request):
 	
 	# Set the project ID
 	PROJECT_ID = os.environ['GCP_PROJECT']
-
+	
 	# Get function env variable
 	NONCE = os.environ.get('NONCE', 'Specified environment variable is not set.')
 	
@@ -26,23 +26,22 @@ def main(request):
 	credentials = google.oauth2.service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_KEY_FILE)
 
 	# Build cloudresourcemanager REST API python object
-	service = discovery.build('cloudresourcemanager','v1', credentials=credentials)
+	service_r = discovery.build('cloudresourcemanager','v1', credentials=credentials)
 	
 	# Service account 
-	sa = f'c1-check@{PROJECT_ID}.iam.gserviceaccount.com'
+	sa = f'serviceAccount:c1-access@{PROJECT_ID}.iam.gserviceaccount.com'
 
 	get_iam_policy_request_body = {}
 	
 	roles =[]
-	pri =[]
 	per =[]
 	msg = ''
 	err=''
 	try:
-		response = service.projects().getIamPolicy(resource=PROJECT_ID, body=get_iam_policy_request_body).execute()["bindings"]
-		for r in response:
-			if sa in r['members']:
-				roles.append(r['role'])
+		bindings = service_r.projects().getIamPolicy(resource=PROJECT_ID, body=get_iam_policy_request_body).execute()["bindings"]
+		for r in bindings:
+			if sa in r["members"]:
+				roles.append(r["role"])
 	except Exception as e: 
 		per =[]
 		msg ='There is an error'
@@ -51,39 +50,18 @@ def main(request):
 		msg='Not least privilege role, please try again!'
 	else:
 		msg='Congratulations! You got the least privileges role. '
+		
+		
+	# Build iam  REST API python object
+	service_i = discovery.build('iam','v1', credentials=credentials)
+
+	
+	try:
+		per = service_i.roles().get(name=roles[0]).execute()["includedPermissions"]
+		
+		
+	except Exception as e: 
+		per =[]
+		err = str(e)
 	
 	return render_template('c1-check.html',  per=per, msg=msg, rn=roles[0], err=err)
-
-
-	# # Build cloudresourcemanager REST API python object
-	# service = discovery.build('iam','v1', credentials=credentials)
-
-
-
-	# name = f'projects/{PROJECT_ID}/roles/c1_access_role_{NONCE}'  
-
-	# per=[]
-	# rolename=''
-	# err =''
-	# try:
-	# 	role = service.projects().roles().get(name=name).execute()
-	# 	rolename = role['name']
-	# 	#print(roles['name'])
-	# 	per = role['includedPermissions']
-		
-	# except Exception as e: 
-	# 	per =[]
-	# 	err = str(e)
-	
-	# msg='Congratulations! You get the least privileges. '
-	
-	# if len(per)!=len(pri):
-	# 	msg='Not least privilege, please try again!'
-		
-	# else:
-	# 	for p in pri:
-	# 		if p not in per:
-	# 			msg='Not least privilege, please try again!'
-	# 			return render_template('c1-check.html',  per=per, msg=msg, rn=rolename, err=err)
-	
-	# return render_template('c1-check.html',  per=per, msg=msg, rn=rolename, err=err)
