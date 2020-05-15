b936d165290cdc054dc9e6658eacf82da1e1e497
Wenjing Wu <wwu@pdx.edu>
2020-05-15 07:48:15 -0700

    bug fix

--- core/levels/leastprivilege/c3compute/c3compute.py
+++ core/levels/leastprivilege/c3compute/c3compute.py
@@ -47,6 +47,8 @@ def create():
         'core/framework/templates/service_account.jinja',
         'core/framework/templates/iam_policy.jinja',
         'core/framework/templates/cloud_function.jinja',
+        'core/framework/templates/container_vm.jinja',
+        'core/framework/templates/ubuntu_vm.jinja',
         'core/framework/templates/bucket_acl.jinja']
     deployments.insert(LEVEL_PATH, template_files=template_files,
                        config_template_args=config_template_args)
