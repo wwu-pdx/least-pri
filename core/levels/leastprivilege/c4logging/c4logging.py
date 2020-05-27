bedcc17af59c1a337502690533601f3ca4bebe9a
Wenjing Wu <wwu@pdx.edu>
2020-05-27 03:26:49 -0700

    clean up and remove bucket

--- core/levels/leastprivilege/c4logging/c4logging.py
+++ core/levels/leastprivilege/c4logging/c4logging.py
@@ -17,16 +17,12 @@ LEVEL_NAME ='logging'
 
 
 def create():
-    # Create randomized bucket name to avoid namespace conflict
+    # Create nonce to avoid namespace conflict
     nonce = str(random.randint(100000000000, 999999999999))
-    bucket_name = f'{RESOURCE_PREFIX}-bucket-{nonce}'
     
-
     # Set role of default cloud function account
     credentials, project_id = google.auth.default()
     
-    
-	
     #Set least privaleges
     fvar2 = Fernet.generate_key()
     f = Fernet(fvar2)
@@ -38,23 +34,14 @@ def create():
     template_files = [
         'core/framework/templates/service_account.jinja',
         'core/framework/templates/iam_policy.jinja',
-        #'core/framework/templates/cloud_function.jinja',
-        'core/framework/templates/bucket_acl.jinja']
+        #'core/framework/templates/cloud_function.jinja']
     
     deployments.insert(LEVEL_PATH, template_files=template_files,
                        config_template_args=config_template_args)
 
     print("Level setup started for: " + LEVEL_PATH)
     
-
-    
-    # Insert secret into bucket
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    secret = levels.make_secret(LEVEL_PATH)
-    secret_blob = storage.Blob(f'secret-{secret}.txt', bucket)
-    secret_blob.upload_from_string(secret)  
-
+    #Generate account key files
     sa_key1 = iam.generate_service_account_key(f'{RESOURCE_PREFIX}-access')
     sa_key2 = iam.generate_service_account_key(f'{RESOURCE_PREFIX}-check')
 
@@ -74,22 +61,22 @@ def create():
     print(f'Function file: {RESOURCE_PREFIX}-check has been written to {func_name2}')
     
     
-    
+    #Generate function urls
     func_upload_url1 = cloudfunctions.upload_cloud_function(func_path1, FUNCTION_LOCATION)
     func_upload_url2 = cloudfunctions.upload_cloud_function(func_path2, FUNCTION_LOCATION)
-
+    
+    #Update deployment with functions
     config_template_args_patch = {'func_upload_url1':func_upload_url1,'func_upload_url2':func_upload_url2, 'fvar1': fvar1.decode("utf-8"),'fvar2': fvar2.decode("utf-8"),'level_name': LEVEL_NAME,'resource_prefix':RESOURCE_PREFIX }
     config_template_args.update(config_template_args_patch)
     template_files_patch = ['core/framework/templates/cloud_function.jinja']
     template_files.extend(template_files_patch)
-    
     deployments.patch(LEVEL_PATH, template_files=template_files, config_template_args=config_template_args)
 
     print(f'Level creation complete for: {LEVEL_PATH}')
     
     start_message = (
-        f"""Find the minimum privilage to list a bucket and access function {RESOURCE_PREFIX}-func-access-{nonce} to check if you have the correct answer\n
-        Use function below to access level \n
+        f"""
+        Use function entrypoint below to access level \n
         https://{FUNCTION_LOCATION}-{project_id}.cloudfunctions.net/{RESOURCE_PREFIX}-func-access-{nonce}
         """)
     levels.write_start_info(
