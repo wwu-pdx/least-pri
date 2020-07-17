e0b3b803f572bdb62c5aab634718d470c6dee55a
Wenjing Wu <wwu@pdx.edu>
2020-07-17 09:56:52 -0700

    add cloudfunciton logging

--- core/framework/project.py
+++ core/framework/project.py
@@ -121,7 +121,8 @@ def setup_project():
     services_logtypes = {"storage.googleapis.com":"all",
                          "compute.googleapis.com":"all",
                          "logging.googleapis.com":["DATA_READ"],
-                         "iam.googleapis.com":"all"}
+                         "iam.googleapis.com":"all",
+                         "cloudfunctions.googleapis.com":"all"}
     confirmed = 'y' == input(
             f'Turn on audit logging for selected services {services_logtypes}? [y/n]: ').lower().strip()[0]
     if(confirmed):
